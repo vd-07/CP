9477d1514bcfda0a03161dd6f5f8b38f0091831d
Vivek Dubey <dubey.vivek.2805@gmail.com>
2019-10-14 22:54:09 +0530

    new upload

---
+++ DSA/ads.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <iostream>
+
+ using namespace std;
+ int main()
+ {
+     int x=0;
+     if(x==1)
+     if(x==0)
+     cout<<"vd";
+     else
+     cout<<"cd";
+     else 
+     cout<<"bd";
+     return 0;
+ }
